232d436b1e5933ec4f99999923c1345b9d408ab6
Aleksandr <oxil91l3v@gmail.com>
2019-08-19 01:33:13 +0300

    almost ready

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+    #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -15,19 +15,26 @@ config = {
     "SUCCESSFUL_PERCENT": 0.95
 }
 
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from datetime import datetime
 import argparse
 import os
 import json
+import logging
+import re
+import gzip
+import statistics
+
+
+last_log = namedtuple('last_log', ['date', 'path'])
 
 
 def parse_path():
     parser = argparse.ArgumentParser()  # argument_default='./log_analyzer_config'
-    parser.add_argument('--config_path')
+    parser.add_argument('--config_path', default='./log_analyzer_config')
     args = parser.parse_args()
     # print(type(args.config_path))
-    print(args.config_path)
+    # print(args.config_path)
     return args.config_path
 
 
@@ -44,16 +51,115 @@ def merge_config(default_config, ext_config):
     return default_config
 
 
-def find_last_file(path):
-    return open('./log/nginx-access-ui.log-20170630')
+def init_logging(config):
+    loging_path = config.get('LOGGING_FILE')
+
+    logging.basicConfig(
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y.%m.%d %H:%M:%S',
+        filename=loging_path
+    )
 
 
-def read_line():
-    pass
+def find_last_file(config):
+    pattern = re.compile(r'^nginx-access-ui\.log-(?P<date>\d{8})(\.gz)?$')
+    root_dir = config.get("LOG_DIR")
+    last_file = None
+    date_last_file = None
+    for path, dirs, files in os.walk(root_dir):
+        for filename in files:
+            match_file = re.match(pattern, filename)
+            date = match_file.group(1)
+            date = datetime.strptime(date, "%Y%m%d")
+
+            if not last_file or date_last_file < date:
+                last_file = os.path.join(path, filename)
+                date_last_file = date
+
+    logging.info('Found last log. Date: {}, Name: {}'.format(date_last_file, last_file))
+    return last_log(date_last_file, last_file)
+    #return open('./log/nginx-access-ui.log-20170630')
 
 
-def read_log():
-    pass
+def report(date, cfg):
+    date = datetime.strftime(date, '%Y.%m.%d')
+    report_path = os.path.join(cfg.get('REPORT_DIR'), 'report_{}'.format(date))
+    if os.path.exists(report_path):
+        FileExistsError('report at {} exist'.format(report_path))
+
+    return report_path
+
+def split_line(line):
+    try:
+        data = line.split(' ')
+        #url, request_time = line[7].strip(), float(line[-1].strip())
+        return {'url': data[7], 'request_time': data[-1]}
+    except Exception:
+        logging.debug(f"Error while split line: \n\t {line}")
+        return None
+
+
+def reader_log_file(path, cfg):
+    with gzip.open(path, 'rb') if path.endswith('.gz') else open(path) as file:
+        lines = [line.rstrip() for line in file]
+        count_lines = len(lines)
+        succ_count_lines = 0
+        for line in lines:
+            data = split_line(line)
+            #print(type(data))
+            if not data:
+               continue
+
+            yield data
+            succ_count_lines += 1
+            #print(succ_count_lines)
+
+        curr_succ_pers = succ_count_lines / count_lines
+        print(curr_succ_pers)
+        logging.info("successful percent = {}".format(curr_succ_pers))
+
+
+        #lines = [line.split()  for line in lines]
+        #print(lines[0:2])
+
+        if curr_succ_pers < cfg.get('SUCCESSFUL_PERCENT'):
+            raise RuntimeError('Too many incorrect data')
+
+
+def calculate_statistics(lines_gen, cfg):
+    full_request_time = 0
+    full_url_count = 0
+    data = defaultdict(list)
+    for i in lines_gen:
+        url, req_time = i['url'], float(i['request_time'])
+        #print(url)
+
+        data[url].append(req_time)
+        full_request_time += req_time
+        full_url_count += 1
+
+    stat = []
+    for k, v in data.items():
+        stat.append({
+            'url': k,
+            'count': len(v),
+            'count_perc': 100 * len(v) / full_url_count,
+            'time_sum': sum(v),
+            'time_perc': 100 * sum(v) / full_request_time,
+            'time_avg': sum(v) / len(v),
+            'time_max': max(v),
+            'time_med': statistics.median(v)
+        })
+
+    sorted_stat = (sorted(stat, key=lambda k: k["time_sum"], reverse=True) )[:cfg['REPORT_SIZE']]
+
+    return json.dumps(sorted_stat)
+
+
+def write_report(stat, path):
+    with open(path, mode='tw', encoding='utf-8') as report:
+        report.write(stat)
 
 
 def main():
@@ -61,21 +167,21 @@ def main():
         "REPORT_SIZE": 1000,
         "REPORT_DIR": "./reports",
         "LOG_DIR": "./log",
-        "SUCCESSFUL_PERCENT": 0.95
+        "SUCCESSFUL_PERCENT": 0.95,
+        "LOGGING_FILE": "./logging_file"
     }
 
+    init_logging(default_config)
     path = parse_path()
     ext_config = read_log_analyzer_config(path)
     config = merge_config(default_config, ext_config)
-    log_file = find_last_file(config.get("LOG_DIR"))
+    #init_logging(config)
 
-    # потом сделать более красивую функцию
-
-    lines = [line.rstrip() for line in log_file]
-    lines = [line.split() for line in lines]
-
-    print(lines[0])
-
+    log_file = find_last_file(config)
+    report_path = report(log_file.date, config)
+    lines = reader_log_file(log_file.path, config)
+    stat = calculate_statistics(lines, config)
+    write_report(stat, report_path)
 
 
 if __name__ == "__main__":
